1cae14c1c7d0bc6581670a5fa163c0c08f13693b
Troy <troyd9216@gmail.com>
2021-09-30 22:47:05 -0500

    updated basic winston

--- config/winston.js
+++ config/winston.js
@@ -1,9 +1,54 @@
 const appRoot = require('app-root-path')
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston')
+const { combine, splat, timestamp, printf, simple, label } = format
 
+const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
+    let msg = `${timestamp} [${level}] : ${message}`
+    if (metadata) {
+        msg += JSON.stringify(metadata)
+    }
+    return msg
+})
 
-winston.add(new winston.transports.File({ filename: 'report.log', level: 'info', handleException: true, maxsize: 5242880, maxFiles: 10 }))
+const options = {
+    file: {
+        level: 'debug',
+        name: 'file.info',
+        filename: `${appRoot}/logs/app.log`,
+        handleException: true,
+        json: true,
+        maxsize: 5242880,
+        maxFiles: 100,
+        colorize: true
+    },
+    console: {
+        level: 'debug',
+        handleException: true,
+        json: false,
+        colorize: true
+    },
+}
 
+const logger = new createLogger({
+    format: combine(
+        label({ label: 'CUSTOM', message: true }),
+        timestamp(),
+        simple()
+    ),
+    format: combine(
+        format.colorize(),
+        splat(),
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new transports.Console(options.console),
+        new transports.File(options.file),
 
-winston.info('Info message');
-winston.log('Hi this is a test', 'info')
+    ],
+})
+
+logger.info('HII')
+logger.error('Errors!')
+
+module.exports = logger
--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ module.exports = (db) => {
         const driverName = req.body.driver_name;
         const driverVehicle = req.body.driver_vehicle;
 
+
         if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
             return res.send({
                 error_code: 'VALIDATION_ERROR',
